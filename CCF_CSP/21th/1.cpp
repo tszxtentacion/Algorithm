60e1c600a00d7c57a09df5f8efb8611a4dfb55fa
tszxtentacion <77733785+tszxtentacion@users.noreply.github.com>
2023-03-12 23:12:23 +0800

    Create 1.cpp
    nt模拟

---
+++ CCF_CSP/21th/1.cpp
@@ -0,0 +1,25 @@
+/* CSP202012-1 期末预测之安全指数 */
+
+#include <stdio.h>
+
+#define N 100000
+int n, w[N], score[N];
+
+int main()
+{
+    /* 读入数据 */
+    int i;
+    scanf("%d", &n);
+    for (i = 0; i < n; i++)
+        scanf("%d%d", &w[i], &score[i]);
+
+    /* 计算 */
+    int ans = 0;
+    for (i = 0; i < n; i++)
+        ans += w[i] * score[i];
+
+    /* 输出结果 */
+    printf("%d\n", ans > 0 ? ans : 0);
+
+    return 0;
+}
