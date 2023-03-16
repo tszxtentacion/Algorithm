90d2124805aa406778cd1c8da441b050e2bc9000
tszxtentacion <2321266807@qq.com>
2023-03-16 20:18:35 +0800

    D题目更加简洁的解法

--- Lanqiao/13th/D.py
+++ Lanqiao/13th/D.py
@@ -1,15 +1,5 @@
-n, m = int(input("")), int(input(""))
-total = 0
-
-num_list = []
-for i in range(1, n + 1):
-    sumv = sum(list(map(int, list(str(i)))))
-    if len(num_list) != 0:
-        for j in range(0, len(num_list)):
-            if sumv <= sum(list(map(int, list(str(j))))) or j == len(num_list) - 1:
-                num_list.insert(j + 1, i)
-                break
-    else:
-        num_list.append(i)
-
-print(num_list[m-1])
+n = int(input())
+m = int(input())
+s = list(range(1, n+1))
+s.sort(key=lambda x: sum(int(i) for i in str(x)))
+print(s[m-1])
