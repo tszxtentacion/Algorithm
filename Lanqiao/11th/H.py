4e888cb274c0dd68d5501e8d2605b2d1e95e71f2
tsz <2321266807@qq.com>
2023-03-28 10:30:12 +0800

    dp

---
+++ Lanqiao/11th/H.py
@@ -0,0 +1,17 @@
+n = int(input())
+dp = [list(map(int, input().split())) for i in range(n)]
+
+# 初始化边界条件
+for i in range(1, n):
+    dp[i][0] = dp[i-1][0]
+    dp[i][i] += dp[i-1][i-1]
+
+for i in range(2, n):
+    for j in range(1, i):
+        dp[i][j] += max(dp[i-1][j], dp[i-1][j-1])
+
+# 考虑走法的次数限制：取dp数组中间位置即可
+if n % 2 == 0:
+    print(max(dp[n-1][n//2], dp[n-1][n//2-1]))
+else:
+    print(dp[n-1][n//2])
