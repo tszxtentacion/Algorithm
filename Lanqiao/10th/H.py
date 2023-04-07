dee427c9a4119d493a77a74c55543d1e94a7b6bd
tsz <2321266807@qq.com>
2023-04-07 09:21:45 +0800

    数列

---
+++ Lanqiao/10th/H.py
@@ -0,0 +1,16 @@
+n = int(input())
+arr = list(map(int, input().split))
+arr.sort()
+dlist = []
+for i in range(n - 1):
+    d.append(arr[i + 1] - arr[i])
+
+d = min(dlist)
+s = []
+i = 0
+while (1):
+    s.append(arr[0] + d * i)
+    if (s[i] == arr[n-1]):
+        break
+    i += 1
+print(len(s))
